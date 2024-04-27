3257a179b296e8d913fbc10d5ac4277771c10b47
Ujjwal <ujjwalsinghkashyap12@gmail.com>
2024-04-27 11:45:13 +0530

    env and config setup

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 // console.log("hello world");
 import app from "./src/app";
+import { config } from "./src/config/config";
 
 const startServer=()=>{
-    const PORT=process.env.PORT || 3000;
+    // const PORT=process.env.PORT || 3000;
+    const PORT=config.port||3000
     app.listen(PORT,()=>{
         console.log(`Server listening on Port ${PORT}`)
     })
