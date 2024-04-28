cd6fdb8b88c14a86c5f00dc9790a07eca44384c9
Ujjwal <ujjwalsinghkashyap12@gmail.com>
2024-04-28 12:27:21 +0530

    user creation & token generation

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ const _config={
     port:process.env.PORT,
     mongo_uri:process.env.MONGO_URI,
     env:process.env.ENV,
+    jwt_secret:process.env.JWT_SECRET,
     
 }
 
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,9 @@
 import { Request,Response,NextFunction } from "express"
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser=async (req:Request,res:Response,next:NextFunction)=>{
     const { name,email,password }=req.body;
@@ -18,14 +21,29 @@ const createUser=async (req:Request,res:Response,next:NextFunction)=>{
         const error=createHttpError(400,"user already exist with same email");
         return next(error);
     }
+
+
     // 2.logic/prcoess
+    const hashPassword=await bcrypt.hash(password,10);
+    
+    const newUser=await userModel.create({
+        name,
+        email,
+        password:hashPassword
+    });
+
+    const token=sign({ sub: newUser._id },config.jwt_secret as string,{
+        expiresIn:"7d",
+    });
     // 3.response
-
-
-
      res.json({
-        "message":"user registered"
-     });
+        accessToken:token
+     })
+
+
+    //  res.json({
+    //     "message":"user registered"
+    //  });
 }
 
 export {createUser};
