1521600b2d7db06114b621abf04afd7bbeb0807d
Ujjwal <ujjwalsinghkashyap12@gmail.com>
2024-04-28 12:40:13 +0530

    error handling on register user

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,7 +5,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
-
+import { User } from "./userTypes";
 const createUser=async (req:Request,res:Response,next:NextFunction)=>{
     const { name,email,password }=req.body;
 
@@ -15,30 +15,50 @@ const createUser=async (req:Request,res:Response,next:NextFunction)=>{
         const error=createHttpError(400,"All the field are required ");
         return next(error);
     }
-    const user= await userModel.findOne({email});
-    if(user)
+
+    try{
+        const user= await userModel.findOne({email});
+        if(user)
+        {
+            const error=createHttpError(400,"user already exist with same email");
+            return next(error);
+        }
+    }catch(err)
     {
-        const error=createHttpError(400,"user already exist with same email");
-        return next(error);
+        return next(createHttpError(500,"error occured while finding user"));
     }
 
 
     // 2.logic/prcoess
     const hashPassword=await bcrypt.hash(password,10);
-    
-    const newUser=await userModel.create({
-        name,
-        email,
-        password:hashPassword
-    });
+    let newUser: User;
+    try{
+        newUser=await userModel.create({
+            name,
+            email,
+            password:hashPassword
+        });
+    }
+    catch(error)
+    {
+        return next(createHttpError(500,"Error occured while creating user"));
+    }
+   
 
-    const token=sign({ sub: newUser._id },config.jwt_secret as string,{
-        expiresIn:"7d",
-    });
-    // 3.response
-     res.json({
-        accessToken:token
-     })
+    try{
+        const token=sign({ sub: newUser._id },config.jwt_secret as string,{
+            expiresIn:"7d",
+        });
+        // 3.response
+         res.json({
+            accessToken:token
+         })
+    }
+    catch(err)
+    {
+        return next(createHttpError(500,"error occured while JWT token generation"));
+    }
+    
 
 
     //  res.json({
